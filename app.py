70fb5b7c99a568d18bd698160bc847264ced3f36
Mohammad Firdous Alam <mdfirdous10alam@gmail.com>
2024-09-25 21:48:40 +0000

    Started Styling

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from re import DEBUG
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)  #Flask object
 
 @app.route("/")  #when a certain url is requested what should be returned.
 def hello_world():
-  return "Assalamoalikum its working"
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
