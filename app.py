70c71b61fc32ee8de99bb32b309721e1635acebc
Mohammad Firdous Alam <mdfirdous10alam@gmail.com>
2024-09-28 12:19:17 +0000

    fetched data using python

--- app.py
+++ app.py
@@ -2,9 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)  # Flask object
 
+RECIPE = [{
+    'id': 1,
+    'name': 'Pizza',
+    'ingredients': ['cheese', 'tomato', 'dough'],
+    'Time': 30
+}, {
+    'id': 2,
+    'name': 'Burger',
+    'ingredients': ['cheese', 'tomato', 'bun'],
+}, {
+    'id': 3,
+    'name': 'Chowmean',
+    'ingredients': ['chow', 'tomato', 'vinegar'],
+    'Time': 30
+}, {
+    'id': 4,
+    'name': 'Pasta',
+    'ingredients': ['pasta', 'tomato', 'pasta_masala'],
+    'Time': 30
+}]
+
+
 @app.route("/")  # when a certain url is requested what should be returned.
 def hello_world():
-  return render_template('home.html')
+   return render_template('home.html',
+                          recipies=RECIPE,
+                          company_name='MasterChef')
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+   app.run(host="0.0.0.0", debug=True)
